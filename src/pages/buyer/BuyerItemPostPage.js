317b582db68077950d0705682ec099dcb90845ad
whgkswo <gjuk7238@gmail.com>
2024-09-09 11:30:49 +0900

    feat BuyerPages(#35)

--- src/pages/buyer/BuyerItemPostPage.js
+++ src/pages/buyer/BuyerItemPostPage.js
@@ -1,14 +1,58 @@
 import Header from '../../components/header/Header';
 import Sidebar from '../../components/sidebar/Sidebar';
+import ReactTableWithCheckbox from '../../components/Table/ReactTableWithCheckbox';
+import { columnData, data } from '../../data/ManufacturerData';
 
 const BuyerItemPostPage = () => {
+    const BuyerItemContainer = ({ leftContent, rightContent, leftSearch, rightSearch }) => {
+        return (
+            <div className='manufacturer-content-container'>
+            <div className='manufacturer-content'>
+                <div className='manufacturer-content-name'>{leftContent}</div>
+                <input className='manufacturer-content-input'></input>
+                {leftSearch === undefined ? <div></div> : <img className = 'item-search-button' src='icons/zoom.png'></img>}
+            </div>
+            <div className='manufacturer-content'>
+                <div className='manufacturer-content-name'>{rightContent}</div>
+                <input className='manufacturer-content-input'></input>
+                {rightSearch === undefined ? <div></div> : <img className = 'item-search-button' src='icons/zoom.png'></img>}
+            </div>
+        </div>
+        );
+    }
     return (
         <div>
             <Header></Header>
             <div className='app-container'>
                 <Sidebar></Sidebar>
                 <div className='app-content-container'>
-                    <div>바이어 아이템 등록 페이지~</div>
+                    <div className='app-background'>
+                    <div className='manufacturer-input-container'>
+                        <div className='manufacturer-input-header'>
+                            <div className='manufacturer-input-text'>바이어 제품 등록</div>
+                            <button className='manufacturer-input-button'>등록</button>
+                        </div>
+                        <BuyerItemContainer leftContent='바이어' rightContent='공급량'></BuyerItemContainer>
+                        <BuyerItemContainer leftContent='제품명' leftSearch = {true} rightContent='사이즈'></BuyerItemContainer>
+                        <BuyerItemContainer leftContent='제품 코드' rightContent='색상'></BuyerItemContainer>
+                        <BuyerItemContainer leftContent='카테고리' rightContent='공급가'></BuyerItemContainer>
+                        </div>
+                        <div className='manufacturer-list-container'>
+                            <div className='manufacturer-tool-container'>
+                                <select>
+                                    <option disabled='true'>Filter By</option>
+                                    <option>최신순</option>
+                                    <option>무슨순</option>
+                                    <option>무슨순2</option>
+                                </select>
+                                <div className='manufacturer-button-container'>
+                                    <button className='manufacturer-button'>수정</button>
+                                    <button className='manufacturer-button'>삭제</button>
+                                </div>
+                            </div>
+                            <ReactTableWithCheckbox columns={columnData} data={data}></ReactTableWithCheckbox>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/buyer/BuyerPostPage.js
+++ src/pages/buyer/BuyerPostPage.js
@@ -1,6 +1,22 @@
 import Header from "../../components/header/Header";
 import Sidebar from '../../components/sidebar/Sidebar';
+import ReactTableWithCheckbox from "../../components/Table/ReactTableWithCheckbox";
+import { columnData, data } from "../../data/ManufacturerData";
 
+const BuyerContent = ({ leftContent, rightContent }) => {
+    return (
+        <div className='manufacturer-content-container'>
+            <div className='manufacturer-content'>
+                <div className='manufacturer-content-name'>{leftContent}</div>
+                <input className='manufacturer-content-input'></input>
+            </div>
+            <div className='manufacturer-content'>
+                <div className='manufacturer-content-name'>{rightContent}</div>
+                <input className='manufacturer-content-input'></input>
+            </div>
+        </div>
+    );
+}
 const BuyerPostPage = () => {
     return (
         <div>
@@ -8,7 +24,32 @@ const BuyerPostPage = () => {
             <div className='app-container'>
                 <Sidebar></Sidebar>
                 <div className="app-content-container">
-                    <div>바이어 등록 페이지~</div>
+                    <div className="app-background">
+                        <div className='manufacturer-input-container'>
+                            <div className='manufacturer-input-header'>
+                                <div className='manufacturer-input-text'>바이어 정보 등록</div>
+                                <button className='manufacturer-input-button'>등록</button>
+                            </div>
+                            <BuyerContent leftContent='바이어 명' rightContent='담당자 연락처'></BuyerContent>
+                            <BuyerContent leftContent='바이어 코드' rightContent='담당자 이메일'></BuyerContent>
+                            <BuyerContent leftContent='사업자 구분' rightContent='주소'></BuyerContent>
+                        </div>
+                        <div className='manufacturer-list-container'>
+                            <div className='manufacturer-tool-container'>
+                                <select>
+                                    <option disabled='true'>Filter By</option>
+                                    <option>최신순</option>
+                                    <option>무슨순</option>
+                                    <option>무슨순2</option>
+                                </select>
+                                <div className='manufacturer-button-container'>
+                                    <button className='manufacturer-button'>수정</button>
+                                    <button className='manufacturer-button'>삭제</button>
+                                </div>
+                            </div>
+                            <ReactTableWithCheckbox columns={columnData} data={data}></ReactTableWithCheckbox>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
