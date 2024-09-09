56b6209ba66f0395ddbcd52eedb86f58dac2a043
YoungJunRoh <shdudwns59@gmail.com>
2024-09-09 16:05:08 +0900

    OrderPostPageLayout

--- src/components/OrderPost/ProductSearch.js
+++ src/components/OrderPost/ProductSearch.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
-import axios from 'axios';
 
 const ProductSearch = ({ onAddOrder }) => {
     const [searchParams, setSearchParams] = useState({
@@ -12,7 +11,8 @@ const ProductSearch = ({ onAddOrder }) => {
         stock: '',
         contact: '',
         unitPrice: '',
-        quantityUnit: ''
+        quantityUnit: '',
+        contractPeriod: '' // 계약 기간 추가
     });
 
     const handleChange = (e) => {
@@ -20,50 +20,44 @@ const ProductSearch = ({ onAddOrder }) => {
         setSearchParams(prevState => ({ ...prevState, [id]: value }));
     };
 
+    const handleFetch = () => {
+        onAddOrder({
+            ...searchParams,
+            registrationDate: new Date().toISOString().split('T')[0],
+            deliveryDate: ''
+        });
+        setSearchParams({
+            customer: '',
+            productName: '',
+            size: '',
+            customerCode: '',
+            productCode: '',
+            stock: '',
+            contact: '',
+            unitPrice: '',
+            quantityUnit: '',
+            tel: '',
+            item: '',
+            contractPeriod: '' // 계약 기간 초기화
+        });
+    };
+
     const DateRangePicker = () => {
         const [dateRange, setDateRange] = useState([null, null]);
         const [startDate, endDate] = dateRange;
       
         return (
-            <DatePicker className='date-range-picker'
-              selectsRange={true}
-              startDate={startDate}
-              endDate={endDate}
-              onChange={(update) => {
-                setDateRange(update);
-              }}
-              isClearable={true}
-              dateFormat="yyyy/MM/dd"
-              placeholderText="기간 선택"
+            <DatePicker
+                selectsRange
+                startDate={startDate}
+                endDate={endDate}
+                onChange={(update) => setDateRange(update)}
+                isClearable
+                dateFormat="yyyy/MM/dd"
+                placeholderText="기간 선택"
             />
         );
-      };
-    // const handleFetch = async () => {
-    //     try {
-    //         // 예시 URL, 실제 API 엔드포인트로 대체 필요
-    //         const response = await axios.get('/api/orders', {
-    //             params: {
-    //                 customer: searchParams.customer,
-    //                 customerCode: searchParams.customerCode,
-    //             }
-    //         });
-
-    //         // 응답 받은 데이터를 state에 설정
-    //         const fetchedData = response.data;
-    //         setSearchParams(prevState => ({
-    //             ...prevState,
-    //             productName: fetchedData.productName || '',
-    //             size: fetchedData.size || '',
-    //             productCode: fetchedData.productCode || '',
-    //             stock: fetchedData.stock || '',
-    //             contact: fetchedData.contact || '',
-    //             unitPrice: fetchedData.unitPrice || '',
-    //             quantityUnit: fetchedData.quantityUnit || ''
-    //         }));
-    //     } catch (error) {
-    //         console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
-    //     }
-    // };
+    };
 
     return (
         <div className='product-search'>
@@ -73,57 +67,52 @@ const ProductSearch = ({ onAddOrder }) => {
                         <td className='title'>고객사</td>
                         <td>
                             <div className='divSearch'>
-                            <input type='text' id='customer' value={searchParams.customer} onChange={handleChange} />
-                            <button id='searchCustomer' className='search-button'>
-                                <img src='/icons/zoom.png' alt='Search' className='search-icon' />
-                            </button>
+                                <input type='text' id='customer' className = 'orderPostInput' value={searchParams.customer} onChange={handleChange} />
+                                <button id='searchCustomer' className='search-button'>
+                                    <img src='/icons/zoom.png' alt='Search' className='search-icon' />
+                                </button>
                             </div>
-                                {/* <button className='searchCustomer' onClick={handleFetch}></button> */}
                         </td>
                         <td className='title'>제품명</td>
                         <td>
-                        <div className='divSearch'>
-                            <input type='text' id='productName' value={searchParams.productName} onChange={handleChange} />
-                            <button id='searchProduct' className='search-button'>
-                                <img src='/icons/zoom.png' alt='Search' className='search-icon' />
-                            </button>
-                        </div>
+                            <div className='divSearch'>
+                                <input type='text' id='productName' className = 'orderPostInput' value={searchParams.productName} onChange={handleChange} />
+                                <button id='searchProduct' className='search-button'>
+                                    <img src='/icons/zoom.png' alt='Search' className='search-icon' />
+                                </button>
+                            </div>
                         </td>
                         <td className='title'>사이즈</td>
-                        <td><input type='text' id='size' value={searchParams.size} onChange={handleChange} /></td>
+                        <td><input type='text' id='size' className = 'orderPostInput' value={searchParams.size} onChange={handleChange} /></td>
                     </tr>
                     <tr>
                         <td className='title'>계약 기간</td>
-                        <td>
-                        <DateRangePicker></DateRangePicker>
-                        </td>            
+                        <td className='order-date'><DateRangePicker /></td>
                         <td className='title'>제품코드</td>
-                        <td><input type='text' id='productCode' value={searchParams.productCode} onChange={handleChange} /></td>
+                        <td><input type='text' id='productCode' className = 'orderPostInput' value={searchParams.productCode} onChange={handleChange} /></td>
                         <td className='title'>재고량</td>
-                        <td><input type='text' id='stock' value={searchParams.stock} onChange={handleChange} /></td>
+                        <td><input type='text' id='stock' className = 'orderPostInput' value={searchParams.stock} onChange={handleChange} /></td>
                     </tr>
-
                     <tr>
                         <td className='title'>고객코드</td>
-                        <td><input type='text' id='customerCode' value={searchParams.customerCode} onChange={handleChange} /></td>
+                        <td><input type='text' id='customerCode' className = 'orderPostInput' value={searchParams.customerCode} onChange={handleChange} /></td>
                         <td className='title'>품목</td>
-                        <td><input type='text' id='productCode' value={searchParams.productCode} onChange={handleChange} /></td>
+                        <td><input type='text' id='item' className = 'orderPostInput' value={searchParams.item} onChange={handleChange} /></td>
                         <td className='title'>제품 단가</td>
-                        <td><input type='text' id='stock' value={searchParams.stock} onChange={handleChange} /></td>
+                        <td><input type='text' id='unitPrice' className = 'orderPostInput' value={searchParams.unitPrice} onChange={handleChange} /></td>
                     </tr>
                     <tr>
                         <td className='title'>고객사 연락처</td>
-                        <td><input type='text' id='contact' value={searchParams.contact} onChange={handleChange} /></td>
+                        <td><input type='text' id='tel' className = 'orderPostInput' value={searchParams.tel} onChange={handleChange} /></td>
                         <td className='title'>색상</td>
-                        <td><input type='text' id='unitPrice' value={searchParams.unitPrice} onChange={handleChange} /></td>
+                        <td><input type='text' id='color' className = 'orderPostInput' value={searchParams.color} onChange={handleChange} /></td>
                         <td className='title'>수량 단위</td>
-                        <td><input type='text' id='quantityUnit' value={searchParams.quantityUnit} onChange={handleChange} /></td>
+                        <td><input type='text' id='quantityUnit' className = 'orderPostInput' value={searchParams.quantityUnit} onChange={handleChange} /></td>
                     </tr>
                 </tbody>
             </table>
             <div className='button-container'>
-            <button className='load-btn'>불러오기</button>
-                {/* <button className='load-btn' onClick={handleFetch}>불러오기</button> */}
+                <button className='load-btn' onClick={handleFetch}>불러오기</button>
             </div>
         </div>
     );
--- src/pages/OrderManagement/CancelledOrdersPage.js
+++ src/pages/OrderManagement/CancelledOrdersPage.js
@@ -1,17 +1,76 @@
+import React from 'react';
 import Header from '../../components/header/Header';
 import Sidebar from '../../components/sidebar/Sidebar';
+import ReactTable from '../../components/Table/ReactTable';
+import './CancelledOrdersPage.css';
+import DateRangePicker from '../../components/datepicker/DateRangePicker';
 
 const CancelledOrdersPage = () => {
+    // 테이블의 열 정의
+    const columns = React.useMemo(
+        () => [
+            { Header: "선택", accessor: "selection", Cell: () => <input type='checkbox' /> },
+            { Header: "담당자", accessor: "member" },
+            { Header: "주문번호", accessor: "orderId" },
+            { Header: "주문상태", accessor: "orderStatus" },
+            { Header: "등록일", accessor: "registerDate" },
+            { Header: "납기일", accessor: "dueDate" },
+            { Header: "고객사 명", accessor: "customerName" },
+            { Header: "고객 코드", accessor: "customerCode" },
+            { Header: "제품 명", accessor: "productName" },
+            { Header: "수량", accessor: "quantity" },
+            { Header: "금액", accessor: "price" },
+            { Header: "총금액", accessor: "totalPrice" },
+        ],
+        []
+    );
+
+    // 임시 데이터
+    const data = React.useMemo(
+        () => [
+            { selection: false, member: "홍길동", orderId: "12345", orderStatus: "취소됨", registerDate: "2024-09-01", dueDate: "2024-09-15", customerName: "고객사A", customerCode: "C001", productName: "제품A", quantity: 10, price: 50000, totalPrice: 500000 },
+            // 추가 데이터...
+        ],
+        []
+    );
+
+    const handleExportToExcel = () => {
+        console.log("Export to Excel");
+    };
+
+    const handlePrint = () => {
+        window.print();
+    };
+
+    const handleSearch = () => {
+        console.log("Search");
+    };
+
     return (
         <div>
-            <Header></Header>
+            <Header />
             <div className='app-container'>
-                <Sidebar></Sidebar>
+                <Sidebar />
                 <div className='app-content-container'>
-                    <div>반품 내역 페이지~</div>
+                    <div className='date'>
+                        <DateRangePicker></DateRangePicker>
+                        <label>주문 번호</label>
+                        <input type='text' id='productName'/>
+                        <button id='searchProduct' className='search-button'>
+                            <img src='/icons/zoom.png' alt='Search' className='search-icon' />
+                        </button>
+                    </div>
+                    <div className='actions-container'>
+                        <button className='miscBtn' onClick={handleSearch}>조회</button>
+                        <button className='miscBtn' onClick={handleExportToExcel}>엑셀 다운</button>
+                        <button className='miscBtn' onClick={handlePrint}>인쇄</button>
+                    </div>
+                    <h2>취소된 주문</h2>
+                    <ReactTable columns={columns} data={data} />
                 </div>
             </div>
         </div>
     );
 }
+
 export default CancelledOrdersPage;
