b02f217f276db110e081596284452d8fab729d98
JinHyeok Hwang <dujin132@gmail.com>
2024-09-11 18:19:24 +0900

    #80

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -8,8 +8,8 @@ const Sidebar = () => {
             <div className="sidebar">
                 <Dropdown title={'즐겨찾기'} buttons={[
                     {name: '즐겨찾기1', urlTo: '/'}, {name: '즐겨찾기2', urlTo: '/'}, {name: '즐겨찾기3', urlTo: '/'}]} expandedDropdown={expandedDropdown} setExpandedDropdown={setExpandedDropdown}></Dropdown>
-                <Dropdown title={'최근 보고서'} buttons={[
-                    {name: '보고서1', urlTo: '/'}, {name: '보고서2', urlTo: '/'}, {name: '보고서3', urlTo: '/'}]} expandedDropdown={expandedDropdown} setExpandedDropdown={setExpandedDropdown}></Dropdown>
+                {/* <Dropdown title={'최근 보고서'} buttons={[
+                    {name: '보고서1', urlTo: '/'}, {name: '보고서2', urlTo: '/'}, {name: '보고서3', urlTo: '/'}]} expandedDropdown={expandedDropdown} setExpandedDropdown={setExpandedDropdown}></Dropdown> */}
                 <Dropdown title={'주문관리'} buttons={[
                     {name: '주문 등록', urlTo: '/order'}, {name: '주문 승인', urlTo: '/order-approval'}, {name: '반품 내역', urlTo: '/cancelled-order'}]} expandedDropdown={expandedDropdown} setExpandedDropdown={setExpandedDropdown}></Dropdown>
                 <Dropdown title={'제품관리'} buttons={[
