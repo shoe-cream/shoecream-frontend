83fec86f2836b7c8a7138fb930ab352e1997227e
whgkswo <gjuk7238@gmail.com>
2024-09-14 15:23:29 +0900

    feat BuyerItemPatch(#173)

--- src/requests/PatchMultiBuyerItemsRequest.js
+++ src/requests/PatchMultiBuyerItemsRequest.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import Swal from "sweetalert2";
 
-const sendPatchMultiItemRequest = async(state, requestBody, executeAfter) => {
+const sendPatchMultiBuyerItemsRequest = async(state, requestBody, executeAfter) => {
     try{
         console.log('requestBody in request: ', requestBody);
-        const response = await axios.patch('http://localhost:8080/items',
+        const response = await axios.patch('http://localhost:8080/buyer-items',
             requestBody,
             {
                 headers: {
@@ -15,17 +15,17 @@ const sendPatchMultiItemRequest = async(state, requestBody, executeAfter) => {
         );
         if(response.status === 200 || response.status === 201){
             Swal.fire({text: '수정 완료'});
-            console.log('제품 일괄 수정 성공', response);
+            console.log('고객사 단가 일괄 수정 성공', response);
             if(executeAfter !== undefined){
                 executeAfter();
             }
         }else{
-            console.log('제품 일괄 수정 실패: ', response.status);
+            console.log('고객사 단가 일괄 수정 실패: ', response.status);
             Swal.fire({text: `요청 실패(${response.status})`});
         }
     } catch(error){
-        console.error('제품 일괄 수정 실패(에러 발생): ', error);
+        console.error('고객사 단가 일괄 수정 실패(에러 발생): ', error);
         Swal.fire({text: `요청 실패(${error.status})`});
     }
 }
-export default sendPatchMultiItemRequest;
+export default sendPatchMultiBuyerItemsRequest;
