c3412d3efbb9e7a8a124da14f2a2c09441670aae
whgkswo <gjuk7238@gmail.com>
2024-09-15 14:13:27 +0900

    feat ManufactureItemPage(#194)

---
+++ src/requests/GetAllManufacturersRequest.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+const sendGetAllManufacturersRequest = async (state, setData, setIsLoading) => {
+    try {
+        const response = await axios.get(`http://localhost:8080/manufacturers/all`, 
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': state.token
+                }   
+            }
+        );
+        if (response.status === 200) {
+            console.log('모든 제조사 정보 GET요청 성공: ', response.data);
+            setData(response.data);
+            if(setIsLoading !== undefined){
+                setIsLoading(false);
+            }
+        } else {
+            console.log('모든 제조사 정보 GET요청 실패');
+            Swal.fire({text: `요청 실패(${response.status})`});
+        }
+    } catch (error) {
+        console.error('모든 제조사 정보 GET요청 실패(에러 발생)', error);
+        Swal.fire({text: `요청 실패(${error.status})`});
+    }
+};
+
+export default sendGetAllManufacturersRequest;
---
+++ src/requests/GetManufacturerItemsRequest.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+const sendGetManufacturerItemsRequest = async (state, page, size, setData, sort, setIsLoading ) => {
+    try {
+        /* console.log('state:', state.token); */
+        const response = await axios.get(`http://localhost:8080/manufacture-items`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${state.token}`
+            }   
+            ,params: {
+                page: page,
+                size: size,
+                /* sort: sort, */
+            }
+        });
+
+        if (response.status === 200) {
+            console.log('제조사 아이템 정보 GET요청 성공: ', response.data);
+            setData(response.data);
+            if(setIsLoading !== undefined){
+                setIsLoading(false);
+            }
+        } else {
+            console.log('제조사 아이템 정보 GET요청 성공: ', response.status);
+            Swal.fire({text: `요청 실패(${response.status})`});
+        }
+    } catch (error) {
+        console.error('제조사 아이템 정보 GET요청 성공 (에러 발생):', error);
+        Swal.fire({text: `요청 실패(${error.status})`});
+    }
+};
+
+export default sendGetManufacturerItemsRequest;
