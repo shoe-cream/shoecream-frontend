d168059a04ffc3ee9beed5399833c056aab88aa1
whgkswo <gjuk7238@gmail.com>
2024-09-22 23:52:25 +0900

    feat AddErrorMessages(#367)

--- src/requests/PostItemRequest.js
+++ src/requests/PostItemRequest.js
@@ -31,7 +31,13 @@ const sendPostItemRequest = async(state, nameInput, codeInput, unitInput, unitpr
         }
     } catch(error){
         console.error('제품 등록 실패(에러 발생): ', error);
-        Swal.fire({text: `요청 실패(${error.status})`});
+        switch (error.status){
+            case 409:
+                Swal.fire({text: '이미 등록된 제품입니다.'});
+                break;
+            default:
+                Swal.fire({text: `요청 실패(${error.status})`});
+        }
     }
 }
 export default sendPostItemRequest;
--- src/requests/PostManufacturersRequest.js
+++ src/requests/PostManufacturersRequest.js
@@ -23,11 +23,20 @@ const sendPostManufacturersRequest = async(state, requestBody, executeAfter) => 
         }
     } catch(error){
         console.error('제조사 등록 실패(에러 발생): ', error);
+        const errorMessage = error.response.data.message.toLowerCase();
         switch(error.status){
             case 400:
                 Swal.fire({text: '이메일을 올바른 형식으로 입력해주세요'});
                 break;
             case 409:
+                if(errorMessage.includes('mf')){
+                    Swal.fire({text: `이미 등록된 제조사명입니다.`});
+                    break;
+                }
+                if(errorMessage.includes('email')){
+                    Swal.fire({text: `이미 등록된 이메일입니다.`});
+                    break;
+                }
                 Swal.fire({text: `제조사명 또는 이메일이 이미 존재합니다`});
                 break;
             default:
