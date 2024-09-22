2929682aa622982d54440020050c1b13ab742997
whgkswo <gjuk7238@gmail.com>
2024-09-23 00:32:09 +0900

    feat CSSFix(#370)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import './Header.css';
-import '../search/SearchWindow.css';
 import { useAuth } from "../../auth/AuthContext";
 import { useEffect, useState } from "react";
 import sendGetMyInfoRequest from "../../requests/GetMyInfoRequest";
